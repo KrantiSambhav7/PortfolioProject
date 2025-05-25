60d776872c889516103551d6dacb39a557f184ae
KrantiSambhav7 <krantisambhav7@gmail.com>
2025-05-25 08:25:33 +0530

    Portfolio Initialised

---
+++ components/Container.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import clsx from 'clsx'; // or use `classnames`
+
+const Container = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={clsx('max-w-4xl mx-auto bg-white dark:bg-black p-4 md:p-10', className)}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
+
